1e385bb9a8ead08ebf29d3d93f5b3f738509b590
Your Name <you@example.com>
2025-10-03 20:35:31 -0300

    new try

--- sudoku.c
+++ sudoku.c
@@ -108,14 +108,20 @@ List* get_adj_nodes(Node* n){
         }      
       }    
     }    
-      
-    
     return list;
 }
 
 
 int is_final(Node* n){
-    return 0;
+  int i, j;
+  for(i = 0; i < 9; i++){
+    for(j = 0; j < 9; j++){
+      if(n->sudo[i][j] == 0){
+        return 0;
+      }
+    }
+  }
+    return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
